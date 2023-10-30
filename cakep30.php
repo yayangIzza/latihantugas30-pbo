3dbaac0a1967e0434e223c07276da122291c27e6
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 20:00:59 +0700

    Add files via upload

---
+++ cakep30.php
@@ -0,0 +1,14 @@
+<?php
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+echo "<font color='red'>kamu </font>";
+echo "<font color='green'>ngerjain sendiri </font>";
+echo "<font color='yellow'>tugas 17sampe </font>";
+echo "<font color='blue'>latihan 30 ini?</font><br>";
+echo "<font color='blue'>jawab</font>";
+echo "<font color='red'>(yoi/enggak)</font>";
+echo "<font color='black'>yoi</font>";
+
+?>
